967ffdabac561be324babdb7b3000c79c2a0cb8d
Ankita Giroti <ankitagiroti@gmail.com>
2026-05-02 20:51:48 +0530

    Update process.py

--- job_scraper/process.py
+++ job_scraper/process.py
@@ -1,12 +1,22 @@
 from scraper import JobScrapper
 from delay import TimeDelay as td
 from csv_job import WriteJobs
+from dotenv import load_dotenv
+import os
 
 if __name__=="__main__":
 
+    load_dotenv()  # reads the .env file and loads all variables into environment
+
+    CONFIG = {
+        "target_url": os.environ.get("TARGET_URL1"),
+        "website_user_id": os.environ.get("TARGET_URL_UID1"),
+        "website_user_password": os.environ.get("TARGET_URL_PASSWORD1")
+    }
+
     job = JobScrapper()
 
-    job.user_login("https://www.linkedin.com/jobs", "session_key", "session_password")
+    job.user_login(CONFIG['target_url', CONFIG['website_user_id'], CONFIG['website_user_password'])
     td.interval()
     job.search_job("data analyst and data science")
     
@@ -19,4 +29,4 @@ if __name__=="__main__":
     
     write_jobs = WriteJobs()
     write_jobs.write_to_csv(job_data)
-    write_jobs.clean_job_data("./csv_data/linkedin_jobs.csv")
+    write_jobs.clean_job_data("./csv_data/jobs.csv")
